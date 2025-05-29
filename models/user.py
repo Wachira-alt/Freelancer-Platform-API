787a78d8f2a5030decdd8553e3bf48ff997bf364
Dennis Wachira <wachiradenis336@gmail.com>
2025-05-29 21:26:08 +0300

    feat(models): create user model and seet it

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from app.database.base import Base
+from sqlalchemy.orm import relationship
 
 class User(Base):
   __tablename__ = "users"
@@ -8,3 +9,6 @@ class User(Base):
   username = Column(String(50), nullable=False, unique=True)
   email = Column(String(100), nullable=False, unique=True)
   full_name = Column(String(100))
+
+  client = relationship("Client", uselist=False, back_populates="user")
+  freelancer = relationship("Freelancer", back_populates="user", uselist=False)
